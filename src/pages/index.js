ce44d648d6ea77ca4331a643996e08d08d45f07a
Darryl Frost <darryl.frost@gmail.com>
2020-10-18 18:49:04 +0100

    local image upload and cloudinary upload added for blog and projects

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,16 +8,17 @@ import Projects from "../components/Projects"
 import Blogs from "../components/Blogs"
 
 export default ( {data} ) => {
-  const { allStrapiProjects: {nodes: projects}} = data
+  const {
+    allStrapiProjects: {nodes: projects},
+    allStrapiBlogs: {nodes: blogs}
+  } = data
   return (
     <Layout>
       <Hero />
       <Services/>
       <Jobs/>
-      <Projects
-        projects={projects}
-        title="featured projects"
-        showLink />
+      <Projects projects={projects} title="featured projects" showLink />
+      <Blogs blogs={blogs} title="latest articles" showLink />
     </Layout>
     )
 }
@@ -41,6 +42,23 @@ export const query = graphql`
         stack {
           id
           title
+        }
+      }
+    }
+    allStrapiBlogs(sort: {fields: date, order: DESC}, limit: 3) {
+      nodes {
+        slug
+        description
+        date(formatString: "MMM Do, YYYY")
+        id
+        title
+        category
+        image {
+          childImageSharp {
+            fluid {
+              ...GatsbyImageSharpFluid
+            }
+          }
         }
       }
     }
